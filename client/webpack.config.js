338c8ceee19cd363c82f4703158f88abc5367cf4
VernerBorn <vernerborn@gmail.com>
2019-12-06 17:43:50 +0300

    config client WEBPACK

---
+++ client/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env
+  const isProd = mode === 'production'
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public/index.html')
+      })
+    ]
+    if (isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: 'main-[hash:8].css'
+      }))
+    }
+    return plugins
+  }
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
+    ]
+  }
+  return {
+    mode: mode,
+    entry: path.join(__dirname, 'src/index.tsx'),
+
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: isProd ? 'main-[hash:8].js' : 'main.js'
+    },
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'ts-loader']
+        },
+        //  Loading SCSS/SASS
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [...getStyleLoaders(), 'sass-loader']
+        },
+        //  Loading CSS
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders()
+        },
+      ]
+    },
+
+    plugins: getPlugins(),
+
+    devServer: {
+      open: true
+    }
+  }
+};
