f5a32aad7b6deeed51d19b761e76d18a4b75c53b
VernerBorn <vernerborn@gmail.com>
2019-12-07 14:23:00 +0300

    add mixingArrayNumber

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Tiles from '../Tiles/Tiles'
 
-const arrayTiles = [1, 2, 3, 4, 5, 6, 7, 8, 0].sort(function(){
+
+const arrayTiles = [1, 2, 3, 4, 5, 6, 7, 8, 0].sort(function () {
   return Math.random() - 0.5;
 });
 
-// const arrayTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
 
 const Board: React.FC = (): JSX.Element => {
   const renderTiles: JSX.Element[] = arrayTiles.map((number, i) => <Tiles number={number} index={i} />)
